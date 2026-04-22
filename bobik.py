8c71fc46a6b0115bbe0a8ecdedecf433f9232523
EgorIv <Chvoe1337@gmail.com>
2026-04-23 01:10:45 +0300

    added newsletter and parse

--- bobik.py
+++ bobik.py
@@ -1,20 +1,53 @@
 import telebot
 from currency import Currency
 from apscheduler.schedulers.background import BackgroundScheduler
+import xml.etree.ElementTree as ET
+import requests
 
-
-TOKEN = "8742389615"
 bot = telebot.TeleBot(TOKEN)
 scheduler = BackgroundScheduler()
+url = "https://www.cbr.ru/scripts/XML_daily.asp"
+users = set()
+
+exchange_rate = {}
 
 
-exchange_rate = [{"CharCode": "USD", "Value": "76.454", "Name": "Dollar"}, {"CharCode": "EUR", "Value": "89.34543", "Name": "Euro"}]
+def update_rates():
+    response = requests.get(url)
+    root = ET.fromstring(response.text)
+    exchange_rate.clear()
+    for valute in root.findall("Valute"):
+        code = valute.find("CharCode").text
+        exchange_rate[code] = Currency(
+            code,
+            valute.find("Name").text,
+            valute.find("Value").text.replace(",", ".")
+        )
+    print(exchange_rate)
+update_rates()
+
 @bot.message_handler(content_types=['text'])
 def get_text_message(message):
-    user_input = message.text.strip()
-    for item in exchange_rate:
-        if item.get("CharCode") == user_input or item.get("Name") == user_input:
-            bot.send_message(message.from_user.id, Currency(item.get("CharCode"), item.get("Name"), item.get("Value")).show_currency())
-            
+    user_input = message.text.strip().upper()
+    users.add(message.chat.id) 
 
+    if exchange_rate[user_input]:
+        bot.send_message(message.from_user.id, exchange_rate[user_input].show_currency())
+
+
+#рассылка в 12 часов
+def send_to_users():
+    usd = exchange_rate["USD"]
+    eur = exchange_rate["EUR"]
+    text = f"{usd.show_currency()}\n{eur.show_currency()}"
+    print(text)
+    print(users)
+    for user in users:
+        bot.send_message(user, text)
+
+
+scheduler.add_job(update_rates, 'cron', hour=12, minute=0)
+scheduler.add_job(send_to_users, 'cron', hour=12, minute=0)
+scheduler.start()
+        
 bot.polling(none_stop=True, interval=0)
--- currency.py
+++ currency.py
@@ -2,7 +2,7 @@ class Currency:
     def __init__(self, char_code, name, value):
         self.char_code = char_code
         self.name = name
-        self.value = value
+        self.value = float(value)
 
     def show_currency(self):
-        return f"{self.char_code}\n{self.name}\n{round(float(self.value), 2)} ₽"
+        return f"{self.char_code}\n{self.name}\n{round(self.value, 2)} ₽"
